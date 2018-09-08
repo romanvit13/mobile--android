e7f081834b7f84ea387dd480481dfe6972da1ef7
= <=>
2018-09-08 14:13:57 +0300

    add validations

--- app/src/main/java/com/vit/roman/roman_vit_app/MainActivity.java
+++ app/src/main/java/com/vit/roman/roman_vit_app/MainActivity.java
@@ -1,42 +1,112 @@
 package com.vit.roman.roman_vit_app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String nameText;
+    private MaterialEditText firstNameEdit;
+    private MaterialEditText lastNameEdit;
+    private MaterialEditText emailEdit;
+    private MaterialEditText phoneEdit;
+    private MaterialEditText passwordEdit;
+    private MaterialEditText passwordConfirmEdit;
+
+    private String firstNameText;
+    private String lastNameText;
+    private String emailText;
+    private String phoneText;
+    private String passwordText;
+    private String passwordConfirmText;
+    private String errorText;
+
+    private Button submitButton;
+
+    //private TextView errorList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.button);
-        ImageButton clearButton = findViewById(R.id.clear_button);
-        final MaterialEditText materialEditText = findViewById(R.id.materialEditText);
-        final TextView nameTextView = findViewById(R.id.name_text);
+        initViews();
 
-        button.setOnClickListener(new View.OnClickListener() {
+        submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                nameText = materialEditText.getText().toString();
-                nameTextView.setText(nameText);
+                initText();
+                register();
             }
         });
+    }
 
-        clearButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                materialEditText.setText("");
-            }
-        });
+    private void initViews() {
+        submitButton = findViewById(R.id.button);
+        firstNameEdit = findViewById(R.id.firstNameEditText);
+        lastNameEdit = findViewById(R.id.lastNameEditText);
+        emailEdit = findViewById(R.id.emailEditText);
+        phoneEdit = findViewById(R.id.phoneEditText);
+        passwordEdit = findViewById(R.id.passwordEditText);
+        passwordConfirmEdit = findViewById(R.id.passwordConfirmEditText);
+    }
 
+    private void initText() {
+        firstNameText = firstNameEdit.getText().toString();
+        lastNameText = lastNameEdit.getText().toString();
+        emailText = emailEdit.getText().toString();
+        phoneText = phoneEdit.getText().toString();
+        passwordText = passwordEdit.getText().toString();
+        passwordConfirmText = passwordConfirmEdit.getText().toString();
+        errorText = "";
+    }
+
+    private void register() {
+        if (!validate()) {
+            Toast.makeText(getApplicationContext(), "Registration has failed!",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            onSignUpSuccess();
+        }
+    }
+
+    private void onSignUpSuccess() {
+        Intent intent = new Intent(MainActivity.this, RegisteredActivity.class);
+        startActivity(intent);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+        if (!Validation.isNameValid(firstNameText)) {
+            firstNameEdit.setError("Please, enter valid first name");
+            errorText += "Invalid firstName";
+            valid = false;
+        }
+        if (!Validation.isNameValid(lastNameText)) {
+            lastNameEdit.setError("Please, enter valid last name");
+            errorText += "Invalid lastName";
+            valid = false;
+        }
+        if (!Validation.isEmailValid(emailText)) {
+            emailEdit.setError("Please, enter valid email");
+            errorText += "Invalid email";
+            valid = false;
+        }
+        if (!Validation.isPhoneValid(phoneText)) {
+            phoneEdit.setError("Please, enter valid phone");
+            valid = false;
+        }
+        if (!Validation.isPasswordValid(passwordText) || !(passwordText.equals(passwordConfirmText))) {
+            passwordEdit.setError("Please, enter valid password");
+            passwordConfirmEdit.setError("Please, enter valid password");
+            errorText += "Invalid password";
+            valid = false;
+        }
+        return valid;
     }
 }
