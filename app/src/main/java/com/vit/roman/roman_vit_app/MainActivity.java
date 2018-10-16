c6329bb74572b9ac096bf9e2f37b2d0982df9b27
= <=>
2018-10-16 21:26:48 +0300

    minor fixes

--- app/src/main/java/com/vit/roman/roman_vit_app/MainActivity.java
+++ app/src/main/java/com/vit/roman/roman_vit_app/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.vit.roman.roman_vit_app.model.User;
+
 
 public class MainActivity extends AppCompatActivity {
 
